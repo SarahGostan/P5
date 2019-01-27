b911e0d06f3f47df8fe82f80ddd15d76ea43d802
unknown <sarahgstn@gmail.com>
2019-01-27 21:47:00 +0100

    Correctif connexion à la BDD

--- model/SongsManager.php
+++ model/SongsManager.php
@@ -5,25 +5,23 @@ require_once("Manager.php");
 class SongsManager extends Manager{
 	
 	public function getFavSongsId($userId){
-		$db = $this->dbConnect();
-		$req = $db->prepare('SELECT song_id FROM users_songs WHERE user_id = ?');
-		$req->execute(array($userId));
-		$favSongsId = $req->fetchAll();
+		$query = $this->db->prepare('SELECT song_id FROM users_songs WHERE user_id = ?');
+		$query->execute(array($userId));
+		$favSongsId = $query->fetchAll();
 		return $favSongsId;
 	}
 	
-	public function getFavSongs($favSongs){
+	 public function getFavSongs($favSongs){
 		$db = $this->dbConnect();
-	$req = $db->query('SELECT song_id FROM users_songs WHERE user_id = 1');
-	while ($favsongs = $req->fetch()){
-		echo($favsongs['song_id']);
-		$fav = $bdd->prepare('SELECT name FROM songs WHERE id = ?');
-		$fav->execute(array($favsongs['song_id']));
-		while($favSongsName = $fav->fetch()){
-			return $favSongsName;
+		$req = $db->query('SELECT song_id FROM users_songs WHERE user_id = 1');
+		while ($favsongs = $req->fetch()){
+			echo($favsongs['song_id']);
+			$fav = $bdd->prepare('SELECT name FROM songs WHERE id = ?');
+			$fav->execute(array($favsongs['song_id']));
+			while($favSongsName = $fav->fetch()){
+				return $favSongsName;
+			}
 		}
-	}
-
-	} 
+	}  
 	
 }
